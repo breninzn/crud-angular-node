8caa87e32cea4ab4c6c30a2497020649c5714cee
breninzn <breno.icpb@gmail.com>
2020-08-15 18:42:03 -0300

    products module

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,8 +1,19 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
+import { ProductsModule } from './products/products.module';
 
 
-const routes: Routes = [];
+const routes: Routes = [
+  {
+    path: '',
+    pathMatch: 'full',
+    redirectTo: 'products'
+  },
+  {
+    path: 'products', 
+    loadChildren: () => import('./products/products.module').then(m => m.ProductsModule)
+  },
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
